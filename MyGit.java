db1c359f55ba353b98b98c9f0dfde14fd07d4798
Junfeng Huang <junfengh@andrew.cmu.edu>
2018-01-20 16:15:32 +1030

    ss

---
+++ MyGit.java
@@ -0,0 +1,12 @@
+class MyGit {
+    public static void myGit() {
+    	Date date = new Date();
+    SimpleDateFormat dateFormatter = new SimpleDateFormat("January 20, 2018");
+    System.out.println("Date: " + dateFormatter.format(date));
+        System.out.println("bumble");
+        System.out.println("I love Cloud Computing");
+    }
+    public static void main(String[] args) {
+        myGit();
+    }
+}
