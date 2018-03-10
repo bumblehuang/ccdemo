b3b4cbc63583621fff5ae8bda621e22e11fa4f88
Junfeng Huang <junfengh@andrew.cmu.edu>
2018-03-10 23:05:41 +1030

    yes

--- MyGit.java
+++ MyGit.java
@@ -1,6 +1,6 @@
 class MyGit {
     public static void myGit() {
-        System.out.println("oo3");
+        System.out.println("oo322");
         System.out.println("I love Cloud Computing");
     }
     public static void main(String[] args) {
