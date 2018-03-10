0d9f0aedad6220d3e51c090ed79bc309ee2bf96d
Junfeng Huang <junfengh@andrew.cmu.edu>
2018-03-10 22:46:15 +1030

    a very descriptive commit message

--- MyGit.java
+++ MyGit.java
@@ -1,9 +1,6 @@
 class MyGit {
     public static void myGit() {
-    	Date date = new Date();
-    SimpleDateFormat dateFormatter = new SimpleDateFormat("January 20, 2018");
-    System.out.println("Date: " + dateFormatter.format(date));
-        System.out.println("bumblehuang");
+        System.out.println(your_name);
         System.out.println("I love Cloud Computing");
     }
     public static void main(String[] args) {
