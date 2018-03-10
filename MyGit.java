63ed6f46a4b3c07d02a1e800cde7cf1b3ea44ba0
Junfeng Huang <junfengh@andrew.cmu.edu>
2018-03-10 22:47:05 +1030

    a very descriptive commit message

--- MyGit.java
+++ MyGit.java
@@ -1,6 +1,6 @@
 class MyGit {
     public static void myGit() {
-        System.out.println(your_name);
+        System.out.println("oo");
         System.out.println("I love Cloud Computing");
     }
     public static void main(String[] args) {
